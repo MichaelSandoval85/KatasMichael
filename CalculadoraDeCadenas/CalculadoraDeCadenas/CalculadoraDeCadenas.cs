248577d95da836587ad366b9829d713e99918797
Michael Dariam Sandoval Lancheros <michael.sandoval@sinco.com.co>
2025-10-24 10:19:17 -0500

    🟥🟥🟥 Prueba 2 Cadena Con Un solo numero fallida

--- CalculadoraDeCadenas/CalculadoraDeCadenas/CalculadoraDeCadenas.cs
+++ CalculadoraDeCadenas/CalculadoraDeCadenas/CalculadoraDeCadenas.cs
@@ -16,7 +16,19 @@ public class CalculadoraDeCadenas
         //Assert
         resultado.Should().Be(0);
     }
-    
+
+    [Fact]
+    public void Si_Envio_Un_Numero_Retorno_El_Mismo_Numero()
+    {
+        //Arrange
+        string cadena = "6";
+        
+        //Act
+        int  resultado = ValidarCadena(cadena);
+        
+        //Assert
+        resultado.Should().Be(6);
+    }
     private int ValidarCadena(string cadena)
     {
         return 0 ;
