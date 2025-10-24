774a26ee63da517dba1b976d14c8e2cad52cd9d9
Michael Dariam Sandoval Lancheros <michael.sandoval@sinco.com.co>
2025-10-24 09:56:43 -0500

    🟥🟥🟥 Prueba 1 Cadena Vacia

---
+++ CalculadoraDeCadenas/CalculadoraDeCadenas/CalculadoraDeCadenas.cs
@@ -0,0 +1,24 @@
+﻿using AwesomeAssertions;
+
+namespace CalculadoraDeCadenas;
+
+public class CalculadoraDeCadenas
+{
+    [Fact]
+    public void Si_Envio_Cadena_Vacia_Retorno_0()
+    {
+        //Arrange
+        string cadena = "";
+        
+        //Act
+        int resultado = ValidarCadena(cadena);
+        
+        //Assert
+        resultado.Should().Be(0);
+    }
+
+    private int ValidarCadena(string cadena)
+    {
+        throw new NotImplementedException();
+    }
+}
