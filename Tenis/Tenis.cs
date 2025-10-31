e02046850f302dca96197b8a16f557237045fac5
Maicol Sandoval <maicolsandoval55@gmail.com>
2025-10-30 20:05:13 -0500

    🟥🟥🟥 Envio dos numeros enteros y retornor dos numeros enteros fallando

---
+++ Tenis/Tenis.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+
+namespace Tenis;
+
+public class Tenis
+{
+    [Fact]
+    public void Envio_2_Numeros_Enteros_Devuelve_2_Numeros_Enteros()
+    {
+        //Arrange
+        var numero1 = 1;
+        var numero2 = 2;
+        
+        //Act
+        var resultado = ValidarPuntaje(numero1,numero2);
+        
+        //Assert
+        resultado.Should().Be((1,2));
+    }
+
+    private object ValidarPuntaje(int numero1, int numero2)
+    {
+        //return (1,2);
+        throw new NotImplementedException();
+    }
+}
